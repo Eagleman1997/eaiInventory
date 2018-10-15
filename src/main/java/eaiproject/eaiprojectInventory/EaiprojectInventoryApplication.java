c2e26c993706df50a9c6d3ff816fda567d6a9074
adrianmathys <adrianmathys@adrianscbookpro.edu.ds.fhnw.ch>
2018-10-15 11:01:15 +0200

    init

---
+++ src/main/java/eaiproject/eaiprojectInventory/EaiprojectInventoryApplication.java
@@ -0,0 +1,12 @@
+package eaiproject.eaiprojectInventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaiprojectInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EaiprojectInventoryApplication.class, args);
+	}
+}
---
+++ src/test/java/eaiproject/eaiprojectInventory/EaiprojectInventoryApplicationTests.java
@@ -0,0 +1,16 @@
+package eaiproject.eaiprojectInventory;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EaiprojectInventoryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
