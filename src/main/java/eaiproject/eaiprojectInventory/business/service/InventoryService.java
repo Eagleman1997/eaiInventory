aba040d24afe507882b34ffdc300830b54834d71
Lukas <Lukas@DESKTOP-PC5RKCS>
2018-10-22 16:25:05 +0200

    New Repository / Service

--- src/main/java/eaiproject/eaiprojectInventory/business/service/InventoryService.java
+++ src/main/java/eaiproject/eaiprojectInventory/business/service/InventoryService.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import eaiproject.eaiprojectInventory.data.domain.Inventory;
+import eaiproject.eaiprojectInventory.data.domain.Shampoo;
 import eaiproject.eaiprojectInventory.data.repository.InventoryRepository;
 
 public class InventoryService {
@@ -12,6 +13,9 @@ public class InventoryService {
 	@Autowired
 	private InventoryRepository inventoryRepository;
 	
+	/**
+	 * Invenvory
+	 */
 	public Inventory createInventory(Integer inventory_id, Integer shampoo_id, Integer amount_of_stock, String location) {
 		Inventory inventory = new Inventory(inventory_id, shampoo_id, amount_of_stock, location);
 		return inventoryRepository.save(inventory);
@@ -26,5 +30,24 @@ public class InventoryService {
 		inventory.setInventory_id(Integer.parseInt(inventory_id));
 		return inventoryRepository.save(inventory);
 	}
+	
+	/**
+	 * Shampoo
+	 */
+	public Shampoo createShampoo(Integer shampoo_id, String name, String brand, String type, Double price) {
+		Shampoo shampoo = new Shampoo(shampoo_id, name, brand, type, price);
+		return inventoryRepository.save(shampoo);
+	}
+	
+	public Shampoo readShampooById(String shampooId) {
+		return inventoryRepository.findShampoosByShampooId(Integer.parseInt(shampooId));
+	}
+	
+	public Shampoo updateShampoo(String shampoo_id, String name, String brand, String type, Double price) {
+		Shampoo shampoo = new Shampoo(Integer.parseInt(shampoo_id), name , brand, type, price);
+		shampoo.setShampoo_id(Integer.parseInt(shampoo_id));
+		return inventoryRepository.save(shampoo);
+	}
+	
 
 }
--- src/main/java/eaiproject/eaiprojectInventory/data/repository/InventoryRepository.java
+++ src/main/java/eaiproject/eaiprojectInventory/data/repository/InventoryRepository.java
@@ -6,10 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
 import eaiproject.eaiprojectInventory.data.domain.Inventory;
+import eaiproject.eaiprojectInventory.data.domain.Shampoo;
 
 
 public interface InventoryRepository extends JpaRepository<Inventory, Integer>{
 
 	public List<Inventory> findInventoriesByTrackingId(@Param("inventory_id") Integer InventoryId);
 	public List<Inventory> findInventoriesByOrderId(@Param("OrderId") Integer OrderId);
+	public Shampoo findShampoosByShampooId(@Param("shampoo_id") Integer ShampooId);
+	public Shampoo save(Shampoo shampoo);
 }
